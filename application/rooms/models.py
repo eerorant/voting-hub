71e0a285adb9f5111990b0159343b259f5c365cc
eerorant <eero.rantala@helsinki.fi>
2020-02-21 22:52:21 +0200

    Removed redundant code

--- application/rooms/models.py
+++ application/rooms/models.py
@@ -22,29 +22,3 @@ class Room(db.Model):
     def is_authenticated(self):
         return True
     
-    """
-    #Returns the name of the room as a string
-    @staticmethod
-    def get_room_name(id):
-        stmt = text("SELECT room.name FROM room"
-                     " WHERE (room.id = :id)").params(id=id)
-        res = db.engine.execute(stmt)
-        response = []
-        #This is stupid but I couldn't get it to work any other way
-        for row in res:
-            response.append({"name":row[0]})
-
-        return response[0].get("name")
-    
-    @staticmethod
-    def get_room_id(name):
-        stmt = text("SELECT room.id FROM room"
-                     " WHERE (room.name = :name)").params(name=name)
-        res = db.engine.execute(stmt)
-        response = []
-        #This is stupid but I couldn't get it to work any other way
-        for row in res:
-            response.append({"id":row[0]})
-
-        return response[0].get("id")
-    """
